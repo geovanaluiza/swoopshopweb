c347baf52c01a2e241c6f859b8c96880187001d0
geovanaluiza <geovana.alb@outlook.com>
2026-07-14 15:52:16 -0700

    update video

--- components/BigPhotoSection.vue
+++ components/BigPhotoSection.vue
@@ -1,5 +1,33 @@
 <template>
   <section class="big-photo" aria-hidden="true">
-    <img src="/life-2.jpg" alt="" loading="lazy" />
+    <iframe
+      src="https://www.youtube.com/embed/Ye1KgGUCREs?autoplay=1&mute=1&loop=1&playlist=Ye1KgGUCREs&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3"
+      allow="autoplay; encrypted-media"
+      allowfullscreen
+      title="Northwest University"
+    ></iframe>
   </section>
 </template>
+
+<style scoped>
+.big-photo {
+  position: relative;
+  width: 100%;
+  height: 60vh;
+  min-height: 360px;
+  max-height: 720px;
+  overflow: hidden;
+  background: var(--nu-powder);
+}
+.big-photo iframe {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+  pointer-events: none;
+}
+@media (min-width: 960px) {
+  .big-photo { height: 75vh; min-height: 520px; }
+}
+</style>
